3352e8c458df9ee98cc30eee330c6f57399ea0f3
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-22 23:03:09 -0600

    Se crea la ruta para 'GestorPage'

--- src/pages/hotel/GestorPage.jsx
+++ src/pages/hotel/GestorPage.jsx
@@ -1,6 +1,6 @@
 import { HotelForm } from '../../components/HotelForm'
 
-export const HotelPage = () => {
+export const GestorPage = () => {
     return (
         <div className="min-h-screen bg-gray-50 p-8 flex flex-col items-center font-sans">
             <h2 className="text-4xl font-extrabold text-teal-700 mb-8 select-none border-b-4 border-teal-300 pb-2 w-full max-w-6xl text-center">
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,12 @@ import { LoginPage } from "./pages/login/LoginPage"
 import { RegisterPage } from "./pages/register/registerPage"
 import { HotelPage } from "./pages/hotel/HotelPage"
 import { HotelInfo } from "./components/hotel/HotelInfo"
+import { GestorPage } from "./pages/hotel/GestorPage"
 
 export const routes = [
     {path:'/',element:<HotelPage/>},
     {path:'/register',element:<RegisterPage/>},
     {path:'/login',element:<LoginPage/>},
-    {path:'/hotel/:hotelId', element:<HotelInfo/>}
+    {path:'/hotel/:hotelId', element:<HotelInfo/>},
+    {path:'/hotels', element:<GestorPage/>}
 ]
