eef8e6b076b57cd3e0cb4c1869642e43b16a9687
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-22 23:58:46 -0600

    Se crea la ruta para 'NotFoundPage'

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,13 @@ import { RegisterPage } from "./pages/register/registerPage"
 import { HotelPage } from "./pages/hotel/HotelPage"
 import { HotelInfo } from "./components/hotel/HotelInfo"
 import { GestorPage } from "./pages/hotel/GestorPage"
+import { NotFoundPage } from "./pages/notfound/NotFoundPage"
 
 export const routes = [
     {path:'/',element:<HotelPage/>},
     {path:'/register',element:<RegisterPage/>},
     {path:'/login',element:<LoginPage/>},
     {path:'/hotel/:hotelId', element:<HotelInfo/>},
-    {path:'/hotels', element:<GestorPage/>}
+    {path:'/hotels', element:<GestorPage/>},
+    {path:'*', element:<NotFoundPage/>}
 ]
